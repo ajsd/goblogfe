bb3b319da0bd960f09f050686be84ec060250013
Arunjit Singh <opensrc@ajsd.in>
2013-07-03 23:04:08 +0530

    stop if request method is OPTIONS

--- blogfe/server.go
+++ blogfe/server.go
@@ -27,5 +27,8 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Access-Control-Allow-Origin", corsOrigins)
 	w.Header().Set("Access-Control-Allow-Headers", allowedCORSHeaders)
+	if r.Method == "OPTIONS" {
+		return
+	}
 	s.h.ServeHTTP(w, r)
 }
